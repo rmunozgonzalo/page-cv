5dfa636b5d67341f442a7988c651427184bca7ae
rmunozgonzalo <munoz.rojas.gonzalo@gmail.com>
2021-01-23 12:55:20 -0300

    contactform enable

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\MailerInterface;
+
+class ContactController extends AbstractController
+{
+     /**
+      * @Route("/contactpost", name="contactPost")
+      */
+    public function contactAction(Request $request, MailerInterface $mailer)
+    {
+      $name = $request->get('name');
+      $email = $request->get('email');
+      $message = $request->get('message');
+      $from = $this->getParameter('app.email');
+      $owner = $this->getParameter('app.email.owner');
+      $message = (new Email())
+                     ->from($from)
+                     ->to($email)
+                     ->subject('Contacto')
+                     ->html($this->renderView('Emails/emailContacto.html.twig',array('name' => $name)),'text/html');
+
+     $messageOwner = (new Email())
+                    ->from($from)
+                    ->to($owner)
+                    ->subject('Contacto')
+                    ->html($this->renderView('Emails/emailPropietario.html.twig',array('name' => $name,'message'=>$message,'email'=>$email)),'text/html');
+
+      $result = $mailer->send($message);
+      $result2 = $mailer->send($messageOwner);
+
+      $contents = $this->renderView('gallery/gallery.html.twig', [
+      ]);
+
+      return new Response($contents);
+    }
+}
