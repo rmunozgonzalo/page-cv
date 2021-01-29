8001e74d8f140edcfc4361b23755b33c1dae9567
rmunozgonzalo <munoz.rojas.gonzalo@gmail.com>
2021-01-29 20:57:16 -0300

    add entity gallery for admin

---
+++ src/Entity/Gallery.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="gallery")
+ */
+class Gallery
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titulo;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $descripcion;
+
+    public function __construct()
+    {
+    }
+
+    public function getId(){
+      return $this->id;
+    }
+
+    public function getTitulo(){
+      return $this->titulo;
+    }
+
+    public function setTitulo($titulo){
+      $this->titulo = $titulo;
+    }
+
+    public function getRefreshToken(){
+      return $this->descripcion;
+    }
+
+    public function setDescripcion($descripcion){
+      $this->descripcion = $descripcion;
+    }
+
+}
