d59ae70f4ec58502b5bbe3d87eeaf693d797ef48
rmunozgonzalo <munoz.rojas.gonzalo@gmail.com>
2021-01-29 21:56:30 -0300

    config upload img big and mini

--- src/Entity/Gallery.php
+++ src/Entity/Gallery.php
@@ -2,9 +2,11 @@
 
 namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 /**
  * @ORM\Entity
+ * @Vich\Uploadable
  * @ORM\Table(name="gallery")
  */
 class Gallery
@@ -26,8 +28,95 @@ class Gallery
      */
     private $descripcion;
 
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $image;
+
+    /**
+     * @Vich\UploadableField(mapping="images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $image_mini;
+
+    /**
+     * @Vich\UploadableField(mapping="images", fileNameProperty="image_mini")
+     * @var File
+     */
+    private $imageFile_mini;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    private $updatedAt;
+
     public function __construct()
     {
+    }
+
+    public function setImageFileMini(File $imageFile_mini = null)
+    {
+        $this->imageFile_mini = $imageFile_mini;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($imageFile_mini) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFileMini()
+    {
+        return $this->imageFile_mini;
+    }
+
+    public function setImageMini($image_mini)
+    {
+        $this->image_mini = $image_mini;
+    }
+
+    public function getImageMini()
+    {
+        return $this->image_mini;
+    }
+
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
     }
 
     public function getId(){
@@ -49,5 +138,15 @@ class Gallery
     public function setDescripcion($descripcion){
       $this->descripcion = $descripcion;
     }
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
 
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
 }
