44213fe820be4ce1c9ae2faaebdfa2f166018d0b
rmunozgonzalo <munoz.rojas.gonzalo@gmail.com>
2021-01-30 13:40:31 +0000

    update comandos y workflow

--- comandos.sh
+++ comandos.sh
@@ -1,5 +1,9 @@
+sudo chown admin:admin var/ -R
+sudo chown admin:admin public/upload/images/ -R
+git pull origin main
 composer install
-sudo php bin/console cache:clear --env=prod
-sudo php bin/console cache:clear --env=dev
-sudo php bin/console doctrine:schema:update --force
+php bin/console cache:clear --env=prod
+php bin/console cache:clear --env=dev
+php bin/console doctrine:schema:update --force
 sudo chown www-data:www-data var/ -R
+sudo chown www-data:www-data public/upload/images/ -R
