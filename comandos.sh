89d1d2e8ab11e48f2ab98f573a2fcb8bf63f1a55
rmunozgonzalo <munoz.rojas.gonzalo@gmail.com>
2021-01-30 10:02:40 -0300

    update comandos.sh

--- comandos.sh
+++ comandos.sh
@@ -1,5 +1,5 @@
 composer install
-php bin/console cache:clear --env=prod
-php bin/console cache:clear --env=dev
-php bin/console doctrine:schema:update --force
+sudo php bin/console cache:clear --env=prod
+sudo php bin/console cache:clear --env=dev
+sudo php bin/console doctrine:schema:update --force
 sudo chown www-data:www-data var/ -R
